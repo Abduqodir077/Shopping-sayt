2aa11fa4b1685097880afd3038e69d2f26984593
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-07-17 13:10:41 +0500

    js yozsh kk

--- js.js
+++ js.js
@@ -1,3 +1,20 @@
 // const obj = {name: "John", age: 30, city: "New York"};
 
 // const myJSON = JSON.stringify(obj);
+
+// ------------
+
+
+// let local = document.querySelector(".local_page");
+// let span = document.querySelector(".ili");
+// let btn = document.querySelector(".sss");
+
+// btn.addEventListener("click", function () {
+//     local.style.transform = "translateX(0px)";
+// });
+// span.addEventListener("click", function () {
+//     local.style.transform = "translateX(450px)"
+// });
+
+
+// ------------
